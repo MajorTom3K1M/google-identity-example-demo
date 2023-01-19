2d0f3bd16d0c8b6937fbabd097aab5607fd44d9c
unknown <invaderzgame@gmail.com>
2023-01-19 15:44:00 +0700

    add demo in to project

--- server.js
+++ server.js
@@ -8,5 +8,13 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 
+app.get("/demo1", function (req, res) {
+  res.sendFile(path.join(__dirname, "/demo/demo1.html"));
+});
+
+app.get("/demo2", function (req, res) {
+  res.sendFile(path.join(__dirname, "/demo/demo2.html"));
+});
+
 app.listen(port);
 console.log("Server started at http://localhost:" + port);
